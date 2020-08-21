6e006d5b8c0f7084a2f5cdaf39ad5608732d36d7
Alejandro Montes de Oca <la.montesdeoca.p@gmail.com>
2020-08-20 22:36:02 -0500

    Genere tres endpoint's: Home, 2 nuevo proyecto (get, post)

--- routes/index.js
+++ routes/index.js
@@ -1,18 +1,15 @@
 const express = require('express');
 const router = express.Router();
 
+//importar controlador
+const proyectosController = require('../controllers/proyectosController');
 
 module.exports = function() {
 
-    //rutas para el home
-    router.get('/', (req, res) => {
-        res.send('Inicio...');
-    });
-
+    //rutas para la app
+    router.get('/', proyectosController.proyectosHome );
+    router.get('/nuevo-proyecto', proyectosController.formularioProyecto)
+    router.post('/nuevo-proyecto', proyectosController.nuevoProyecto);
     
-    router.get('/nosotros', (req, res) => {
-        res.send('Nosotros...');
-    });
-
     return router;
 }
