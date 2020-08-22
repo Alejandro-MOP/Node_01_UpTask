9b98f9b1556d56b4d6b70203d5d3264c7578ead5
Alejandro Montes de Oca <la.montesdeoca.p@gmail.com>
2020-08-22 16:33:06 -0500

    Funcion asincrona para insertar proyectos en MySQL

--- controllers/proyectosController.js
+++ controllers/proyectosController.js
@@ -1,3 +1,6 @@
+const Proyectos = require('../models/Proyectos');
+const slug = require('slug');
+
 exports.proyectosHome = (req, res) => {
 
     res.render('index', {
@@ -12,7 +15,7 @@ exports.formularioProyecto = (req, res) => {
     });
 }
 
-exports.nuevoProyecto = (req, res) => {
+exports.nuevoProyecto = async (req, res) => {
     //Enviar a consola console.log(req.body);
     //Validar el input no este vacio
     const { nombre } = req.body;
@@ -25,16 +28,16 @@ exports.nuevoProyecto = (req, res) => {
 
     //si hay errores
     if(errores.length > 0){
+
         res.render('nuevoProyecto', {
             nombrePagina: 'Nuevo Proyecto',
             errores
         });
+
     }else{
-        //si no hay errores
-        
         //Insertar en BD
-
+        //const url = slug(nombre).toLowerCase(); crear la url
+        const proyecto = await Proyectos.create({ nombre });
+        res.redirect('/');            
     }
-
-
 }
