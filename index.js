36091b4830e1b9bfaf8265fdc5d03a4414ec1ad5
Alejandro Montes de Oca <la.montesdeoca.p@gmail.com>
2020-09-19 21:28:45 -0500

    instale dependencias de proyecto para el MVC de usuario

--- index.js
+++ index.js
@@ -4,17 +4,19 @@ const path = require('path');
 const bodyParser = require('body-parser');
 const db = require('./config/db');
 const helpers = require('./helpers');
-
-
+const flash = require('connect-flash');
+const session = require('express-session');
+const cookieParser = require('cookie-parser');
 
 //Connexion BD
 db.sync()
-    .then( () => console.log('Conectado a la BD') )
-    .catch( (error) => console.log(error) );
+.then( () => console.log('Conectado a la BD') )
+.catch( (error) => console.log(error) );
 
 //importar modelo
 require('./models/Proyectos');
 require('./models/Tareas');
+require('./models/Usuarios');
 
 //crear app express
 const app = express();
@@ -25,18 +27,31 @@ app.use(express.static('public'));
 //Habilitar Pug (Template Engine)
 app.set('view engine', 'pug');
 
+//habilitar body parser para leer formularios
+app.use(bodyParser.urlencoded({ extended: true }));
+
 //Añadir la carpeta de las vistas
 app.set('views', path.join(__dirname, './views'));
 
+//Agregar flash messages
+app.use(flash());
+
+//
+app.use(cookieParser());
+
+//Sessions nos permite navegar en varias paginas sin volverse a autenticar
+app.use(session({
+    secret: 'supersecreto',
+    resave: false,
+    saveUninitialized: false
+}));
+
 //funcion vardump en applicacion
 app.use( (req, res, next) =>{
-
     res.locals.vardump = helpers.vardump;
+    res.locals.errores = req.flash();
     next();
 });
-
-//habilitar body parser para leer formularios
-app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use('/', routes() );
 
