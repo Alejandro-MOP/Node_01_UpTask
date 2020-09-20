dbbdc75fdb93cc078bee94b2b49dddab0ad87374
Alejandro Montes de Oca <la.montesdeoca.p@gmail.com>
2020-09-19 21:29:25 -0500

    desarrollo MVC para el modulo de usuarios (CrearCuenta)

---
+++ models/Usuarios.js
@@ -0,0 +1,60 @@
+const Sequelize = require('sequelize');
+const db = require('../config/db');
+const Proyectos = require('./Proyectos');
+const bcrypt = require('bcrypt-nodejs');
+
+const Usuarios = db.define('usuarios', {
+
+    id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+    },
+
+    email: {
+        type: Sequelize.STRING(60),
+        allowNull: false, //campo vacío
+        unique:true,
+        validate: {
+            isEmail: {
+                msg: 'Agrega un Email Válido'
+            },
+            notEmpty: {
+                msg: 'El email no puede estar vácio'
+            },
+            isUnique: (value, next) => {
+                let self = this;
+                Usuarios.findOne( { where: { email: value } } )
+                    .then( usuario => {
+                        if (usuario && self.id !== usuario.id) {
+                            return next('El email ya se encuentra registrado');
+                        }
+                        return next();
+                    })
+                    .catch(error => {
+                        return next(error);
+                    })
+            }
+        },
+    },
+
+    password: {
+        type: Sequelize.STRING(60),
+        allowNull: false,
+        validate: {
+            notEmpty: {
+                msg: 'El password no puede ir vácio'
+            }
+        }
+    }
+}, {
+    hooks: {
+        beforeCreate(usuario){
+            usuario.password = bcrypt.hashSync(usuario.password, bcrypt.genSaltSync(10));
+        }
+    }
+});
+
+Usuarios.hasMany(Proyectos); //Cardinalidad 1 =>muchos
+
+module.exports = Usuarios;
